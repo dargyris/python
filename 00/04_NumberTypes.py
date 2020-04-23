1f3f61e93799aec3c98c39008a9e905c1e3cf3d5
dargyris <diamantisargiris@gmail.com>
2020-04-23 16:24:44 +0300

    Adding simple python scripts.

---
+++ 00/04_NumberTypes.py
@@ -0,0 +1,10 @@
+# This program demonstrates the types of various numbers
+print()
+print("Type of 2:", type(2))
+print("Type of 2.:", type(2.))
+print("Type of 2.1:", type(2.1))
+print("type of 2.123456789123456789123456789", type(2.123456789123456789123456789))
+print("type of 123456789", type(123456789))
+print()
+print("type of \"String\":", type("String"))
+print("type of \"2\":", type("2"))
