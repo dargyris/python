73cdd18cbf7c63368918d82f023df916333cfff2
dargyris <diamantisargiris@gmail.com>
2020-04-24 09:41:53 +0300

    Adding simple python scripts.

---
+++ 00_Basic/14_TupleMethods.py
@@ -0,0 +1,8 @@
+import os
+os.system("clear")
+print("Hello!")
+print("=======================================================")
+print("Printing all the tuple related methods.")
+print("-------------------------------------------------------")
+print(dir(tuple))
+print()
