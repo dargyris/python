6dee08768e10e0f6b6cfe4d9692101a160c73824
dargyris <diamantisargiris@gmail.com>
2020-04-24 08:58:44 +0300

    Adding simple scripts in python.

---
+++ 00_Basic/12_ListMethods.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+print("Hello!")
+print("=========================================================")
+print("Here are the methods available for the \"list\" data type")
+print("as returned from the dir(list) command:")
+print("---------------------------------------------------------")
+print(dir(list))
+print()
